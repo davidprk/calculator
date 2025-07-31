2805fba07abbe8a4841f8cb87e0fa0ea2d12e1c0
David Park <daviddpark.dev@gmail.com>
2025-07-31 01:38:23 -0500

    Starting UI

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operand1, operand2, operator) {
+    
+}
+
+let operand1;
+let operand2;
+let operator;
