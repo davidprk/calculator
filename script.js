78884f425966e229a03668f003ef55fef7d48888
David Park <daviddpark.dev@gmail.com>
2025-07-31 22:55:41 -0500

    Add clear and delete button functionality

--- script.js
+++ script.js
@@ -21,3 +21,44 @@ function operate(operand1, operand2, operator) {
 let operand1;
 let operand2;
 let operator;
+
+// Display logic
+const screenText = document.querySelector(".screen .text");
+const digitBtns = document.querySelectorAll(".symbol");
+
+let currentInput = "0";
+
+function updateScreen() {
+    screenText.textContent = currentInput;
+}
+
+digitBtns.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.textContent;
+        if (currentInput === "0" && value !== ".") {
+            currentInput = value;
+        }
+        else {
+            currentInput += value;
+        }
+        updateScreen();
+    })
+})
+
+const deleteBtn = document.querySelector(".delete");
+
+deleteBtn.addEventListener("click", () => {
+    currentInput = currentInput.slice(0, -1);
+
+    updateScreen();
+});
+
+const clearBtn = document.querySelector(".clear");
+
+clearBtn.addEventListener("click", () => {
+    currentInput = "0";
+
+    updateScreen();
+})
+
+updateScreen();
