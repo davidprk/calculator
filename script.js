b93b51955414f56bf509075c76fe48e39a677dce
David Park <daviddpark.dev@gmail.com>
2025-08-01 18:33:56 -0500

    Add basic operations functionality

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
 function add(a, b) {
-    return a + b;
+    return +a + +b;
 }
 
 function subtract(a, b) {
-    return a - b;
+    return +a - +b;
 }
 
 function multiply(a, b) {
-    return a * b;
+    return +a * +b;
 }
 
 function divide(a, b) {
-    return a / b;
+    return +a / +b;
 }
 
 function operate(operand1, operand2, operator) {
-    if (operator === "+") {
-        return add(operand1, operand2);
-    }
-    else if (operator === "-") {
-        return subtract(operand1, operand2);
-    }
-    else if (operator === "*") {
-        return multiply(operand1, operand2);
-    }
-    else {
-        return divide(operand1, operand2);
+    switch (operator) {
+        case "+": 
+            return add(operand1, operand2);
+        case "-":
+            return subtract(operand1, operand2);
+        case "*":
+            return multiply(operand1, operand2);
+        default:
+            return divide(operand1, operand2);
     }
 }
 
@@ -37,9 +35,14 @@ const screenEquation = document.querySelector(".screen .equation");
 const digitBtns = document.querySelectorAll(".symbol");
 
 let currentInput = "0";
+let currentEquation = "";
 
 function updateScreen() {
     screenText.textContent = currentInput;
+}
+
+function updateEquation() {
+    screenEquation.textContent = currentEquation;
 }
 
 digitBtns.forEach(button => {
@@ -67,11 +70,14 @@ const clearBtn = document.querySelector(".clear");
 
 clearBtn.addEventListener("click", () => {
     currentInput = "0";
+    currentEquation = "";
 
     updateScreen();
+    updateEquation();
 });
 
 updateScreen();
+updateEquation();
 
 //
 // Operation logic
@@ -84,30 +90,43 @@ const operatorBtns = document.querySelectorAll(".operator");
 
 operatorBtns.forEach(button => {
     button.addEventListener("click", (e) => {
-        let clickedOperator = e.target.className;
-        switch (clickedOperator) {
-            case "add":
-                clickedOperator = "+";
-                break;
-            case "subtract":
-                clickedOperator = "-";
-                break;
-            case "multiply":
-                clickedOperator = "*";
-                break;
-            default:
-                clickedOperator = "/";
+        const operations = {
+            "plus operator": "+",
+            "minus operator": "-",
+            "multiply operator": "*",
+            "divide operator": "/"
         }
+        let clickedOperator = operations[e.target.className];
 
         if (operator === "") { // No calculation to perform
             operand1 = currentInput;
             operator = clickedOperator;
-            currentInput += operator;
-
-            updateScreen();
         }
         else { // Operator is non-empty string, chaining operation
-
+            operand2 = currentInput;
+            operand1 = operate(operand1, operand2, operator);
+            operator = clickedOperator;
         }
+
+        currentInput = "";
+        currentEquation = operand1 + operator;
+
+        updateScreen();
+        updateEquation();
     });
+});
+
+const equalBtn = document.querySelector(".equals");
+
+equalBtn.addEventListener("click", () => {
+    operand2 = currentInput;
+    currentInput = operate(operand1, operand2, operator);
+    currentEquation += operand2 + "=";
+
+    operand1 = null;
+    operand2 = null;
+    operator = "";
+
+    updateScreen();
+    updateEquation();
 });
